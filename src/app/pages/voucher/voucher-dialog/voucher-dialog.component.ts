5f9e335f21aa8278bba825bc2711715ab3810fee
hein htet aung <hhein8358@gmail.com>
2024-08-22 12:10:11 +0630

    create voucher dialog

--- src/app/pages/voucher/voucher-dialog/voucher-dialog.component.ts
+++ src/app/pages/voucher/voucher-dialog/voucher-dialog.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatDialogActions, MatDialogRef } from '@angular/material/dialog';
 import { MatDialogContent } from '@angular/material/dialog';
 import { MatDialogTitle } from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-voucher-dialog',
   standalone: true,
-  imports: [MatDialogActions, MatDialogContent, MatFormFieldModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatDialogActions, MatDialogContent, MatFormFieldModule, MatDatepickerModule, MatInputModule, MatDialogTitle, ReactiveFormsModule],
   templateUrl: './voucher-dialog.component.html',
   styles: ``
 })
 export class VoucherDialogComponent {
+
+  form: FormGroup | undefined = undefined;
+  date: any | undefined;
+
+
+  readonly dialogRef = inject(MatDialogRef<VoucherDialogComponent>)
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      customerName: ['', Validators.required],
+      fees: [0, Validators.required],
+      sparepartsExpense: 0,
+      date: Date,
+      remark: ''
+    })
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  calculateNetProfit() {
+    return this.form?.get('fees')?.value - this.form?.get('sparepartsExpense')?.value;
+  }
 
 }
--- src/app/pages/voucher/voucher.component.ts
+++ src/app/pages/voucher/voucher.component.ts
@@ -1,15 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { CategoryComponent } from './category/category.component';
+import { VoucherDialogComponent } from './voucher-dialog/voucher-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-voucher',
   standalone: true,
-  imports: [CategoryComponent],
+  imports: [CategoryComponent, VoucherDialogComponent],
   templateUrl: './voucher.component.html',
   styles: ``
 })
 export class VoucherComponent {
 
+  readonly voucherDialog = inject(MatDialog);
+
+  openVoucherDialog() {
+    this.voucherDialog.open(VoucherDialogComponent);
+  }
   
 }
